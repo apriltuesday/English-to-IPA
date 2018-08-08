01d624880a00b7efcfcc652beaf5fd94d43b4062
April Shen <april.tuesday@gmail.com>
2018-08-08 10:41:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import os
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='eng2ipa',
     version='0.0.1',
-    package_dir={'': '.'},
+    package_dir={'': 'eng_to_ipa'},
     zip_safe=True,
     include_package_data=False,
     description='Converts English text to IPA notation',
